43ced706af508a7c6b9907b150458595fd18601c
controlsjs-team <dev-team@controlsjs.com>
2015-09-07 15:53:31 +0200

    - Changed wineight skin icons image version.

--- src/ng_wineight/wineight.js
+++ src/ng_wineight/wineight.js
@@ -37,7 +37,7 @@ var WinEightControls = {
     'img/we_base.png?2',
     'img/we_hbox.png?2',
     'img/we_vbox.png?1',
-    'img/we_icons.png?3',
+    'img/we_icons.png?4',
 
     // pre-load
     'img/progressline_black.gif?1',
