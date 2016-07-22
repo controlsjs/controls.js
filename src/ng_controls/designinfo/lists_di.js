dee7774ebf9c4887f7a317c39d2e68c852455737
controlsjs-team <dev-team@controlsjs.com>
2016-07-22 16:01:11 +0200

    DesignInfo: Added `ngList`.

--- src/ng_controls/designinfo/lists_di.js
+++ src/ng_controls/designinfo/lists_di.js
@@ -12,7 +12,188 @@
 
 if (typeof ngUserControls === 'undefined') ngUserControls = [];
 ngUserControls['list_designinfo'] = {
-  OnControlDesignInfo: function(def, c, ref)
+  //OnFormEditorInit: function(FE) {},
+  //OnControlDesignInfo: function(def, c, ref) {},
+
+  OnInit: function()
   {
+    if(!ngDESIGNINFO) return;
+
+    function ngListItemsDI()
+    {
+      var di = {
+        DefaultType: 'object',
+        Types: {
+          'object': {
+            DefaultValue: "{}",
+            ObjectProperties: {
+              Text:           { DefaultType: 'string',
+                                Types: {
+                                  'string': {
+                                    InitValue: 'Item'
+                                  }
+                                },
+                                Level: 'basic',
+                                Order: 0.5
+                              },
+              Alt:            { DefaultType: 'string', Order: 0.51 },
+              ID:             { DefaultType: 'undefined',
+                                InitType: 'string',
+                                Order: 0.55
+                              },
+              Checked:        { DefaultType: 'integer',
+                                Level: 'basic',
+                                Order: 0.6,
+                                Types: {
+                                  'integer': {
+                                     DefaultValue: 0,
+                                     InitValue: 1,
+                                     Editor: 'ngfeEditor_DropDownList',
+                                     EditorOptions: {
+                                       Items: [{Value:0,Text:'cbUnchecked'},{Value:1,Text:'chChecked'},{Value:2,Text:'cbGrayed'}]
+                                     }
+                                   }
+                                }
+                              },
+              AllowGrayed:    { DefaultType: 'boolean',
+                                Order: 0.61,
+                                Types: {
+                                  'boolean': {
+                                    DefaultValue: false,
+                                    InitValue: true
+                                  }
+                                }
+                              },
+              Collapsed:      { DefaultType: 'boolean',
+                                Order: 0.62,
+                                Types: {
+                                  'boolean': {
+                                    DefaultValue: false,
+                                    InitValue: true
+                                  }
+                                }
+                              },
+              Visible:        { DefaultType: 'boolean',
+                                Order: 0.65,
+                                Types: {
+                                  'boolean': {
+                                    DefaultValue: true
+                                  }
+                                },
+                                Level: 'basic'
+                              },
+              Enabled:        { DefaultType: 'boolean',
+                                Order: 0.66,
+                                Types: {
+                                  'boolean': {
+                                    DefaultValue: true
+                                  }
+                                },
+                                Level: 'basic'
+                              },
+              RadioGroup:     { DefaultType: 'undefined',
+                                InitType: 'undefined',
+                                Order: 0.67
+                              },
+              H:              { DefaultType: 'undefined',
+                                InitType: 'integer',
+                                Order: 0.7
+                              },
+              MinHeight:      { DefaultType: 'undefined',
+                                InitType: 'integer',
+                                Order: 0.71
+                              },
+              Image:          { DefaultType: 'image', Order: 0.73 },
+              Parent:         { DefaultType: 'undefined',
+                                Order: 0.8,
+                                Types: {
+                                  'object': {},
+                                  'null': {}
+                                },
+                                Level: 'hidden'
+                              },
+              Controls:       { DefaultType: 'undefined',
+                                InitType: 'object',
+                                Order: 0.9
+                              },
+              ControlsHolder: { DefaultType: 'undefined',
+                                Types: {
+                                  'object': {}
+                                },
+                                Level: 'hidden',
+                                Order: 0.91
+                              }
+              // TODO - check API for all properties
+
+              //OnCollapsing
+              //OnCollapsed
+              //OnExpanding
+              //OnExpanded
+            }
+          }
+        }
+      };
+
+      di.Types['object'].ObjectProperties.Items = {
+         DefaultType: 'undefined',
+         InitType: 'array',
+         Level: 'basic',
+         Order: 0.85,
+         Collapsed: true,
+         Types: {
+           'array': {
+             DefaultValue: '[]'
+           }
+         }
+      };
+      ng_SetByRef(di.Types['object'].ObjectProperties.Items, 'ChildDesignInfo', di);
+
+      return di;
+    }
+
+    ngRegisterControlDesignInfo('ngList',function(d,c,ref) {
+      return {
+        ControlCategory: 'Lists',
+        BaseControl: 'ngList',
+        NewControl: {
+          Default: {
+            Properties: {
+              W: { Value: 120 },
+              H: { Value: 180 },
+              Data: {
+                ObjectProperties: {
+                  Items: { Type: 'array',
+                           //Value: [], // TODO - merge arrays
+                           ObjectProperties: {
+                             0: {
+                               Type: 'object',
+                               Value: {
+                                 Text: 'Item 1'
+                               }
+                             }
+                           }
+                         }
+                }
+              }
+            }
+          }
+        },
+        Properties: ng_DIProperties({
+          Data: {
+            Items: { DefaultType: 'array',
+                     Level: 'basic',
+                     Collapsed: false,
+                     ChildDesignInfo: ngListItemsDI(),
+                     Types: {
+                       'array': {
+                         DefaultValue: '[]'
+                       }
+                     }
+                   }
+          }
+        })
+      };
+    });
+
   }
 };
