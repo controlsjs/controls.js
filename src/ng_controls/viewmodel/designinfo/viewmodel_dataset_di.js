5cfbc156081639eaa04f340a2cbbce913a294077
controlsjs-team <dev-team@controlsjs.com>
2016-10-02 13:09:46 +0200

    New DI: ngDataSet

--- src/ng_controls/viewmodel/designinfo/viewmodel_dataset_di.js
+++ src/ng_controls/viewmodel/designinfo/viewmodel_dataset_di.js
@@ -12,8 +12,35 @@
 
 if(typeof ngUserControls === 'undefined') ngUserControls = [];
 var ViewModel_Dataset_DesignInfo = {
-  OnControlDesignInfo: function(def, c, ref)
+  OnInit: function()
   {
+    if(!ngDESIGNINFO) return;
+
+    ngRegisterControlDesignInfo('ngDataSet',function(d,c,ref) {
+      return {
+        Properties: ng_DIProperties({
+          "Data": {
+            "AutoDataSetColumns": ng_DIPropertyBool(false, { Level: 'basic' }),
+            "GetRecordsCommand": ng_DIProperty('string','resetfilters', { Level: 'advanced' }),
+            "SortByVMField": ng_DIProperty('string','SortBy', { Level: 'basic' }),
+            "AllowedSortByVMField": ng_DIProperty('string','AllowedSortBy', { Level: 'basic' }),
+          },
+          "Events": {
+            "OnSetViewModel": ng_DIPropertyEvent('function(c, vm, ovm) {}',{ Level: 'basic' }),
+            "OnReloadDataSet": ng_DIPropertyEvent('function(c) { return true; }',{ Level: 'basic' }),
+            "OnApplyFilters": ng_DIPropertyEvent('function(c) { return true; }',{ Level: 'basic' }),
+            "OnResetFilters": ng_DIPropertyEvent('function(c) { return true; }',{ Level: 'basic' }),
+            "OnSetSortBy": ng_DIPropertyEvent('function(c, sortby) { return true; }',{ Level: 'basic' })
+          },
+          "OverrideEvents": {
+            "OnGetFieldDefValue": ng_DIPropertyEvent('function(c, fd, it, col) { return ""; }',{ Level: 'basic' }),
+            "OnGetRecord": ng_DIPropertyEvent('function(c, it) { return it.Record; }',{ Level: 'basic' }),
+            "OnGetSortBy": ng_DIPropertyEvent('function(c, sortby) { return sortby; }',{ Level: 'basic' }),
+            "OnGetAllowedSortBy": ng_DIPropertyEvent('function(c, allowed) { return allowed; }',{ Level: 'basic' })
+          }
+        })
+      };
+    });
   }
 };
 ngUserControls['viewmodel_dataset_designinfo'] = ViewModel_Dataset_DesignInfo;
