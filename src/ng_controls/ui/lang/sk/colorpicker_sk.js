1b5992d952d9a130a629f7c3adf7f324997f1cf2
controlsjs-team <dev-team@controlsjs.com>
2016-12-06 08:03:46 +0100

    Added missing ui/lang.

---
+++ src/ng_controls/ui/lang/sk/colorpicker_sk.js
@@ -0,0 +1,32 @@
+/*!
+ * Controls.js
+ * http://controlsjs.com/
+ *
+ * Copyright (c) 2014 Position s.r.o.  All rights reserved.
+ *
+ * This version of Controls.js is licensed under the terms of GNU General Public License v3.
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * The commercial license can be purchased at Controls.js website.
+ */
+if(typeof ngc_Lang === 'undefined') ngc_Lang={};
+if(typeof ngc_Lang['sk'] === 'undefined') ngc_Lang['sk']={};
+
+ngc_Lang['sk']['colorpicker_hue']         = 'Odtieň';
+ngc_Lang['sk']['colorpicker_saturation']  = 'Sýtosť';
+ngc_Lang['sk']['colorpicker_value']       = 'Jas';
+ngc_Lang['sk']['colorpicker_satval']      = 'Sýtosť a jas';
+ngc_Lang['sk']['colorpicker_red']         = 'Červená';
+ngc_Lang['sk']['colorpicker_green']       = 'Zelená';
+ngc_Lang['sk']['colorpicker_blue']        = 'Modrá';
+ngc_Lang['sk']['colorpicker_alpha']       = 'Krytie';
+ngc_Lang['sk']['colorpicker_hex']         = 'HTML';
+ngc_Lang['sk']['colorpicker_alphahex']    = 'HTML s krytím';
+ngc_Lang['sk']['colorpicker_from']        = 'Pôvodná farba';
+ngc_Lang['sk']['colorpicker_to']          = 'Nová farba';
+ngc_Lang['sk']['colorpicker_H-SV']        = 'H-SV';
+ngc_Lang['sk']['colorpicker_HSV']         = 'HSV';
+ngc_Lang['sk']['colorpicker_RGB']         = 'RGB';
+
+ngc_Lang['sk']['colorpicker_Submit']='OK';
+ngc_Lang['sk']['colorpicker_Cancel']='Storno';
