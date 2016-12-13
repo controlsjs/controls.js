d963bb49d75d9ab1426b3df80c177e42303d1259
controlsjs-team <dev-team@controlsjs.com>
2016-12-13 16:58:36 +0100

    - Added "ngColorButton" control DI.

--- src/ng_controls/ui/designinfo/colorpicker_di.js
+++ src/ng_controls/ui/designinfo/colorpicker_di.js
@@ -12,6 +12,47 @@
 
 if (typeof ngUserControls === 'undefined') ngUserControls = {};
 ngUserControls['ngColorControls_designinfo'] = {
+  OnFormEditorInit: function(FE)
+  {
+    FormEditor.RegisterPropertyType([
+      {
+        TypeID: 'color_channels',
+        TypeBase: 'object',
+        Name: 'color channels',
+        ShortName: 'chnls',
+        Options: {
+          Add: false,
+          DestroyIfEmpty: true,
+          ObjectProperties: {
+            'H': ng_DIProperty('integer',255,{ Options: { MinValue: 0, MaxValue: 255 }, Level: 'basic' }),
+            'S': ng_DIProperty('float',0,{ Options: { MinValue: 0, MaxValue: 1 }, Level: 'basic' }),
+            'V': ng_DIProperty('float',0,{ Options: { MinValue: 0, MaxValue: 1 }, Level: 'basic' }),
+            'R': ng_DIProperty('integer',255,{ Options: { MinValue: 0, MaxValue: 255 }, Level: 'basic' }),
+            'G': ng_DIProperty('integer',255,{ Options: { MinValue: 0, MaxValue: 255 }, Level: 'basic' }),
+            'B': ng_DIProperty('integer',255,{ Options: { MinValue: 0, MaxValue: 255 }, Level: 'basic' }),
+            'A': ng_DIProperty('float',1,{ Options: { MinValue: 0, MaxValue: 1 }, Level: 'basic' }),
+            'HEX': ng_DIProperty('color_hex6','#ffffff',{ Level: 'basic' }),
+            'HEXA': ng_DIProperty('color_hex8','#ffffffff',{ Level: 'basic' })
+          }
+        }
+      },
+      {
+        TypeID: 'image_bckg',
+        TypeBase: 'object',
+        Name: 'background image',
+        ShortName: 'bgimg',
+        Options: {
+          Add: false,
+          DestroyIfEmpty: true,
+          ObjectProperties: {
+            'L': ng_DIProperty('integer',0,{ Level: 'basic' }),
+            'T': ng_DIProperty('integer',0,{ Level: 'basic' }),
+            'Src': ng_DIProperty('string','http://',{ Level: 'basic' })
+          }
+        }
+      }
+    ]);
+  },
   OnInit: function()
   {
     if(!ngDESIGNINFO) return;
@@ -35,7 +76,18 @@ ngUserControls['ngColorControls_designinfo'] = {
 
     ngRegisterControlDesignInfo('ngColorButton',function(d,c,ref) {
       return {
-        ControlCategory: 'Buttons'
+        ControlCategory: 'Buttons',
+        Properties: ng_DIProperties({
+          'Data': {
+            'Color': ng_DIProperty(['null','color_channels'],null,{ InitType: 'color_channels', Level: 'basic' }),
+            'BackgroundImg': ng_DIProperty(['null','image_bckg'],null,{ InitType: 'image_bckg', Level: 'basic' }),
+            'AllowAlpha': ng_DIPropertyBool(true, { Level: 'basic' })
+          },
+          'Events': {
+            'OnColorChanging': ng_DIPropertyEvent('function(color) { return true; }', { Level: 'basic' }),
+            'OnColorChanged': ng_DIPropertyEvent('function(color) {}', { Level: 'basic' })
+          }
+        })
       };
     });
 
