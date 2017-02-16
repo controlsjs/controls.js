9e382896dd333035c266e7ab624f9d14b8c45e8c
controlsjs-team <dev-team@controlsjs.com>
2017-02-16 11:36:23 +0100

    Added extended country types for English resources.

---
+++ src/ng_basic/lang/en/types_en-ext.js
@@ -0,0 +1,239 @@
+/*!
+ * Controls.js
+ * http://controlsjs.com/
+ *
+ * Copyright (c) 2017 Position s.r.o.  All rights reserved.
+ *
+ * This version of Controls.js is licensed under the terms of GNU General Public License v3.
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * The commercial license can be purchased at Controls.js website.
+ */
+if(typeof ngc_Lang === 'undefined') ngc_Lang={};
+// Locale English - Barbados (en-bb)
+// Locale English - Bermuda (en-bm)
+// Locale English - Bahamas (en-bs)
+// Locale English - Botswana (en-bw)
+// Locale English - Cyprus (en-cy)
+// Locale English - Dominica (en-dm)
+// Locale English - Eritrea (en-er)
+// Locale English - Fiji (en-fj)
+// Locale English - Micronesia, Federated States of (en-fm)
+// Locale English - Grenada (en-gd)
+// Locale English - Ghana (en-gh)
+// Locale English - Gambia (en-gm)
+// Locale English - Guyana (en-gy)
+// Locale English - Kiribati (en-ki)
+// Locale English - Saint Kitts and Nevis (en-kn)
+// Locale English - Cayman Islands (en-ky)
+// Locale English - Saint Lucia (en-lc)
+// Locale English - Liberia (en-lr)
+// Locale English - Lesotho (en-ls)
+// Locale English - Macao (en-mo)
+// Locale English - Malawi (en-mw)
+// Locale English - Namibia (en-na)
+// Locale English - Nigeria (en-ng)
+// Locale English - Papua New Guinea (en-pg)
+// Locale English - Philippines (en-ph)
+// Locale English - Pakistan (en-pk)
+// Locale English - Palau (en-pw)
+// Locale English - Solomon Islands (en-sb)
+// Locale English - Sudan (en-sd)
+// Locale English - Sierra Leone (en-sl)
+// Locale English - South Sudan (en-ss)
+// Locale English - Swaziland (en-sz)
+// Locale English - Turks and Caicos Islands (en-tc)
+// Locale English - Tonga (en-to)
+// Locale English - Trinidad and Tobago (en-tt)
+// Locale English - Saint Vincent and the Grenadines (en-vc)
+// Locale English - Virgin Islands, British (en-vg)
+// Locale English - Vanuatu (en-vu)
+// Locale English - Samoa (en-ws)
+// Locale English - Zambia (en-zm)
+// Locale English - Antigua and Barbuda (en-ag)
+(function(n) { for(var i=n.length-1;i>=0;i--) { var l=n[i];
+if(typeof ngc_Lang[l] === 'undefined') ngc_Lang[l]={};
+ngc_Lang[l]['date_format']               = "dd/MM/yyyy";
+ngc_Lang[l]['date_shortformat']          = "dd/MM/yy";
+ngc_Lang[l]['date_format_parse']         = "d/M/y";
+ngc_Lang[l]['datetime_format']           = "dd/MM/yyyy h:mm:ss a";
+ngc_Lang[l]['datetime_shortformat']      = "dd/MM/yyyy h:mm a";
+ngc_Lang[l]['datetime_format_parse']     = ["d/M/y h:m:s a","d/M/y h:m a"];
+}})(["en-ag","en-zm","en-ws","en-vu","en-vg","en-vc","en-tt","en-to","en-tc","en-sz","en-ss","en-sl","en-sd","en-sb","en-pw","en-pk","en-ph","en-pg","en-ng","en-na","en-mw","en-mo","en-ls","en-lr","en-lc","en-ky","en-kn","en-ki","en-gy","en-gm","en-gh","en-gd","en-fm","en-fj","en-er","en-dm","en-cy","en-bw","en-bs","en-bm","en-bb"]);
+
+// Locale English - Belize (en-bz)
+// Locale English - Cocos (Keeling) Islands (en-cc)
+// Locale English - Cook Islands (en-ck)
+// Locale English - Cameroon (en-cm)
+// Locale English - Christmas Island (en-cx)
+// Locale English - Falkland Islands (Malvinas) (en-fk)
+// Locale English - Guernsey (en-gg)
+// Locale English - Gibraltar (en-gi)
+// Locale English - Ireland (en-ie)
+// Locale English - Isle of Man (en-im)
+// Locale English - British Indian Ocean Territory (en-io)
+// Locale English - Jersey (en-je)
+// Locale English - Kenya (en-ke)
+// Locale English - Madagascar (en-mg)
+// Locale English - Montserrat (en-ms)
+// Locale English - Malta (en-mt)
+// Locale English - Mauritius (en-mu)
+// Locale English - Norfolk Island (en-nf)
+// Locale English - Nauru (en-nr)
+// Locale English - Niue (en-nu)
+// Locale English - Pitcairn (en-pn)
+// Locale English - Rwanda (en-rw)
+// Locale English - Seychelles (en-sc)
+// Locale English - Saint Helena, Ascension and Tristan da Cunha (en-sh)
+// Locale English - Sint Maarten (Dutch part) (en-sx)
+// Locale English - Tokelau (en-tk)
+// Locale English - Tuvalu (en-tv)
+// Locale English - Tanzania, United Republic of (en-tz)
+// Locale English - Uganda (en-ug)
+// Locale English - Anguilla (en-ai)
+(function(n) { for(var i=n.length-1;i>=0;i--) { var l=n[i];
+if(typeof ngc_Lang[l] === 'undefined') ngc_Lang[l]={};
+ngc_Lang[l]['date_format']               = "dd/MM/yyyy";
+ngc_Lang[l]['date_shortformat']          = "dd/MM/yy";
+ngc_Lang[l]['date_format_parse']         = "d/M/y";
+ngc_Lang[l]['time_format']               = "HH:mm:ss";
+ngc_Lang[l]['time_shortformat']          = "HH:mm";
+ngc_Lang[l]['time_format_parse']         = ["H:m:s","H:m"];
+ngc_Lang[l]['datetime_format']           = "dd/MM/yyyy HH:mm:ss";
+ngc_Lang[l]['datetime_shortformat']      = "dd/MM/yyyy HH:mm";
+ngc_Lang[l]['datetime_format_parse']     = ["d/M/y H:m:s","d/M/y H:m"];
+}})(["en-ai","en-ug","en-tz","en-tv","en-tk","en-sx","en-sh","en-sc","en-rw","en-pn","en-nu","en-nr","en-nf","en-mu","en-mt","en-ms","en-mg","en-ke","en-je","en-io","en-im","en-ie","en-gi","en-gg","en-fk","en-cx","en-cm","en-ck","en-cc","en-bz"]);
+
+// Locale English - Belgium (en-be)
+// Locale English - Switzerland (en-ch)
+// Locale English - Germany (en-de)
+// Locale English - Netherlands (en-nl)
+// Locale English - Slovenia (en-si)
+// Locale English - Austria (en-at)
+(function(n) { for(var i=n.length-1;i>=0;i--) { var l=n[i];
+if(typeof ngc_Lang[l] === 'undefined') ngc_Lang[l]={};
+ngc_Lang[l]['decimal_separator']         = ",";
+ngc_Lang[l]['thousands_separator']       = ".";
+ngc_Lang[l]['date_format']               = "dd/MM/yyyy";
+ngc_Lang[l]['date_shortformat']          = "dd/MM/yy";
+ngc_Lang[l]['date_format_parse']         = "d/M/y";
+ngc_Lang[l]['time_format']               = "HH:mm:ss";
+ngc_Lang[l]['time_shortformat']          = "HH:mm";
+ngc_Lang[l]['time_format_parse']         = ["H:m:s","H:m"];
+ngc_Lang[l]['datetime_format']           = "dd/MM/yyyy HH:mm:ss";
+ngc_Lang[l]['datetime_shortformat']      = "dd/MM/yyyy HH:mm";
+ngc_Lang[l]['datetime_format_parse']     = ["d/M/y H:m:s","d/M/y H:m"];
+}})(["en-at","en-si","en-nl","en-de","en-ch","en-be"]);
+
+// Locale English - New Zealand (en-nz)
+// Locale English - Australia (en-au)
+(function(n) { for(var i=n.length-1;i>=0;i--) { var l=n[i];
+if(typeof ngc_Lang[l] === 'undefined') ngc_Lang[l]={};
+ngc_Lang[l]['date_format']               = "d/MM/yyyy";
+ngc_Lang[l]['date_shortformat']          = "d/MM/yy";
+ngc_Lang[l]['date_format_parse']         = "d/M/y";
+ngc_Lang[l]['datetime_format']           = "d/MM/yyyy h:mm:ss a";
+ngc_Lang[l]['datetime_shortformat']      = "d/MM/yyyy h:mm a";
+ngc_Lang[l]['datetime_format_parse']     = ["d/M/y h:m:s a","d/M/y h:m a"];
+}})(["en-au","en-nz"]);
+
+// Locale English - Canada (en-ca)
+if(typeof ngc_Lang['en-ca'] === 'undefined') ngc_Lang['en-ca']={};
+ngc_Lang["en-ca"]['date_format']               = "yyyy-MM-dd";
+ngc_Lang["en-ca"]['date_shortformat']          = "yy-MM-dd";
+ngc_Lang["en-ca"]['date_format_parse']         = "y-M-d";
+ngc_Lang["en-ca"]['datetime_format']           = "yyyy-MM-dd h:mm:ss a";
+ngc_Lang["en-ca"]['datetime_shortformat']      = "yyyy-MM-dd h:mm a";
+ngc_Lang["en-ca"]['datetime_format_parse']     = ["y-M-d h:m:s a","y-M-d h:m a"];
+
+// Locale English - Denmark (en-dk)
+if(typeof ngc_Lang['en-dk'] === 'undefined') ngc_Lang['en-dk']={};
+ngc_Lang["en-dk"]['decimal_separator']         = ",";
+ngc_Lang["en-dk"]['thousands_separator']       = ".";
+ngc_Lang["en-dk"]['date_format']               = "dd/MM/yyyy";
+ngc_Lang["en-dk"]['date_shortformat']          = "dd/MM/yy";
+ngc_Lang["en-dk"]['date_format_parse']         = "d/M/y";
+ngc_Lang["en-dk"]['time_format']               = "HH.mm.ss";
+ngc_Lang["en-dk"]['time_shortformat']          = "HH.mm.ss";
+ngc_Lang["en-dk"]['time_format_parse']         = "H.m.s";
+ngc_Lang["en-dk"]['datetime_format']           = "dd/MM/yyyy HH.mm.ss";
+ngc_Lang["en-dk"]['datetime_shortformat']      = "dd/MM/yyyy HH.mm.ss";
+ngc_Lang["en-dk"]['datetime_format_parse']     = ["H.m.s H","H.m.s .","H.m.s m","H.m.s s"];
+
+// Locale English - Finland (en-fi)
+if(typeof ngc_Lang['en-fi'] === 'undefined') ngc_Lang['en-fi']={};
+ngc_Lang["en-fi"]['decimal_separator']         = ",";
+ngc_Lang["en-fi"]['thousands_separator']       = " ";
+ngc_Lang["en-fi"]['date_format']               = "dd/MM/yyyy";
+ngc_Lang["en-fi"]['date_shortformat']          = "dd/MM/yy";
+ngc_Lang["en-fi"]['date_format_parse']         = "d/M/y";
+ngc_Lang["en-fi"]['time_format']               = "H.mm.ss";
+ngc_Lang["en-fi"]['time_shortformat']          = "H.mm.ss";
+ngc_Lang["en-fi"]['time_format_parse']         = "H.m.s";
+ngc_Lang["en-fi"]['datetime_format']           = "dd/MM/yyyy H.mm.ss";
+ngc_Lang["en-fi"]['datetime_shortformat']      = "dd/MM/yyyy H.mm.ss";
+ngc_Lang["en-fi"]['datetime_format_parse']     = ["H.m.s H","H.m.s .","H.m.s m","H.m.s s"];
+
+// Locale English - Jamaica (en-jm)
+// Locale English - Malaysia (en-my)
+// Locale English - Singapore (en-sg)
+// Locale English - Zimbabwe (en-zw)
+// Locale English - Hong Kong (en-hk)
+(function(n) { for(var i=n.length-1;i>=0;i--) { var l=n[i];
+if(typeof ngc_Lang[l] === 'undefined') ngc_Lang[l]={};
+ngc_Lang[l]['date_format']               = "d/M/yyyy";
+ngc_Lang[l]['date_shortformat']          = "d/M/yy";
+ngc_Lang[l]['date_format_parse']         = "d/M/y";
+ngc_Lang[l]['datetime_format']           = "d/M/yyyy h:mm:ss a";
+ngc_Lang[l]['datetime_shortformat']      = "d/M/yyyy h:mm a";
+ngc_Lang[l]['datetime_format_parse']     = ["d/M/y h:m:s a","d/M/y h:m a"];
+}})(["en-hk","en-zw","en-sg","en-my","en-jm"]);
+
+// Locale English - Israel (en-il)
+if(typeof ngc_Lang['en-il'] === 'undefined') ngc_Lang['en-il']={};
+ngc_Lang["en-il"]['date_format']               = "dd/MM/yyyy";
+ngc_Lang["en-il"]['date_shortformat']          = "dd/MM/yy";
+ngc_Lang["en-il"]['date_format_parse']         = "d/M/y";
+ngc_Lang["en-il"]['time_format']               = "H:mm:ss";
+ngc_Lang["en-il"]['time_shortformat']          = "H:mm";
+ngc_Lang["en-il"]['time_format_parse']         = ["H:m:s","H:m"];
+ngc_Lang["en-il"]['datetime_format']           = "dd/MM/yyyy H:mm:ss";
+ngc_Lang["en-il"]['datetime_shortformat']      = "dd/MM/yyyy H:mm";
+ngc_Lang["en-il"]['datetime_format_parse']     = ["d/M/y H:m:s","d/M/y H:m"];
+
+// Locale English - India (en-in)
+if(typeof ngc_Lang['en-in'] === 'undefined') ngc_Lang['en-in']={};
+ngc_Lang["en-in"]['date_format']               = "dd-MM-yyyy";
+ngc_Lang["en-in"]['date_shortformat']          = "dd-MM-yy";
+ngc_Lang["en-in"]['date_format_parse']         = "d-M-y";
+ngc_Lang["en-in"]['time_format']               = "HH:mm:ss";
+ngc_Lang["en-in"]['time_shortformat']          = "HH:mm";
+ngc_Lang["en-in"]['time_format_parse']         = ["H:m:s","H:m"];
+ngc_Lang["en-in"]['datetime_format']           = "dd-MM-yyyy HH:mm:ss";
+ngc_Lang["en-in"]['datetime_shortformat']      = "dd-MM-yyyy HH:mm";
+ngc_Lang["en-in"]['datetime_format_parse']     = ["d-M-y H:m:s","d-M-y H:m"];
+
+// Locale English - Sweden (en-se)
+if(typeof ngc_Lang['en-se'] === 'undefined') ngc_Lang['en-se']={};
+ngc_Lang["en-se"]['decimal_separator']         = ",";
+ngc_Lang["en-se"]['thousands_separator']       = " ";
+ngc_Lang["en-se"]['date_format']               = "yyyy-MM-dd";
+ngc_Lang["en-se"]['date_shortformat']          = "yy-MM-dd";
+ngc_Lang["en-se"]['date_format_parse']         = "y-M-d";
+ngc_Lang["en-se"]['time_format']               = "HH:mm:ss";
+ngc_Lang["en-se"]['time_shortformat']          = "HH:mm";
+ngc_Lang["en-se"]['time_format_parse']         = ["H:m:s","H:m"];
+ngc_Lang["en-se"]['datetime_format']           = "yyyy-MM-dd HH:mm:ss";
+ngc_Lang["en-se"]['datetime_shortformat']      = "yyyy-MM-dd HH:mm";
+ngc_Lang["en-se"]['datetime_format_parse']     = ["y-M-d H:m:s","y-M-d H:m"];
+
+// Locale English - South Africa (en-za)
+if(typeof ngc_Lang['en-za'] === 'undefined') ngc_Lang['en-za']={};
+ngc_Lang["en-za"]['decimal_separator']         = ",";
+ngc_Lang["en-za"]['thousands_separator']       = " ";
+ngc_Lang["en-za"]['date_format']               = "yyyy/MM/dd";
+ngc_Lang["en-za"]['date_shortformat']          = "yy/MM/dd";
+ngc_Lang["en-za"]['date_format_parse']         = "y/M/d";
+ngc_Lang["en-za"]['datetime_format']           = "yyyy/MM/dd h:mm:ss a";
+ngc_Lang["en-za"]['datetime_shortformat']      = "yyyy/MM/dd h:mm a";
+ngc_Lang["en-za"]['datetime_format_parse']     = ["y/M/d h:m:s a","y/M/d h:m a"];
