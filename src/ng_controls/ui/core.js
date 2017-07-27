cb9f6f51be785c70fe577df79a0879096d4b8108
controlsjs-team <dev-team@controlsjs.com>
2017-07-27 22:17:19 +0200

    Change: Don't create dropdown when InDesignMode.

--- src/ng_controls/ui/core.js
+++ src/ng_controls/ui/core.js
@@ -3206,7 +3206,7 @@ function nge_DoMouseLeave(e, mi)
 
 function nge_DoCreate(d, ref, elm, parent)
 {
-  if((typeof d.DropDown !== 'undefined')&&(typeof this.SetDropDownControl === 'function'))
+  if((typeof d.DropDown !== 'undefined')&&(typeof this.SetDropDownControl === 'function')&&(!this.InDesignMode))
   {
     ng_MergeDef(d.DropDown, {
       L: 0, T: 0,
