fe1eb6e67f1314850db44c383fb162965887d09b
controlsjs-team <dev-team@controlsjs.com>
2025-05-21 13:19:35 +0200

    Removed language from resource filenames.

---
+++ src/ng_basic/lang/cs/types.js
@@ -0,0 +1,34 @@
+/*!
+ * Controls.js
+ * http://controlsjs.com/
+ *
+ * Copyright (c) 2014 Position s.r.o.  All rights reserved.
+ *
+ * This version of Controls.js is licensed under the terms of GNU General Public License v3.
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * The commercial license can be purchased at Controls.js website.
+ */
+if(typeof ngc_Lang === 'undefined') ngc_Lang={};
+// Default locale Czech (cs)
+if(typeof ngc_Lang['cs'] === 'undefined') ngc_Lang['cs']={};
+ngc_Lang['cs']['locale_language']           = "Czech";
+ngc_Lang['cs']['locale_language_native']    = "čeština";
+ngc_Lang['cs']['decimal_separator']         = ",";
+ngc_Lang['cs']['thousands_separator']       = " ";
+ngc_Lang['cs']['date_format']               = "d.M.yyyy";
+ngc_Lang['cs']['date_shortformat']          = "d.M.yy";
+ngc_Lang['cs']['date_format_parse']         = "d.M.y";
+ngc_Lang['cs']['time_format']               = "H:mm:ss";
+ngc_Lang['cs']['time_shortformat']          = "H:mm";
+ngc_Lang['cs']['time_format_parse']         = ["H:m:s","H:m"];
+ngc_Lang['cs']['time_am_pm']                = ["dop.","odp."];
+ngc_Lang['cs']['datetime_format']           = "d.M.yyyy H:mm:ss";
+ngc_Lang['cs']['datetime_shortformat']      = "d.M.yyyy H:mm";
+ngc_Lang['cs']['datetime_format_parse']     = ["d.M.y H:m:s","d.M.y H:m"];
+ngc_Lang['cs']['calendar_months']           = ["leden","únor","březen","duben","květen","červen","červenec","srpen","září","říjen","listopad","prosinec"];
+ngc_Lang['cs']['calendar_months_short']     = ["led","úno","bře","dub","kvě","čvn","čvc","srp","zář","říj","lis","pro"];
+ngc_Lang['cs']['calendar_months_formating'] = ["ledna","února","března","dubna","května","června","července","srpna","září","října","listopadu","prosince"];
+ngc_Lang['cs']['calendar_days']             = ["neděle","pondělí","úterý","středa","čtvrtek","pátek","sobota"];
+ngc_Lang['cs']['calendar_days_short']       = ["ne","po","út","st","čt","pá","so"];
+ngc_Lang['cs']['calendar_days_formating']   = ngc_Lang['cs']['calendar_days'];
