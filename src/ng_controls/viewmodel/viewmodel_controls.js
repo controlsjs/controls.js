16351217faddfffc8f5de929fc8e3964e10febaf
controlsjs-team <dev-team@controlsjs.com>
2025-12-28 17:31:26 +0100

    ngSysDataSetViewModel: Added missing serialization/deserialization of _ActiveFilters values.

--- src/ng_controls/viewmodel/viewmodel_controls.js
+++ src/ng_controls/viewmodel/viewmodel_controls.js
@@ -1122,6 +1122,93 @@ ngUserControls['viewmodel_controls'] = {
       return defs;
     }
 
+    function ngdsvm_SetValues(values,deserialize, valuenames, errors, strictvaluenames)
+    {
+      var ret;
+      if((ng_IsObjVar(values))&&(ng_IsObjVar(values._ActiveFilters))) {
+        var origonsetvalue=this.OnSetValue;
+        try {
+          var self=this;
+          this.OnSetValue=function(c,setval,instance, valpath)
+          {
+            if(origonsetvalue) val=origonsetvalue.apply(self, arguments);
+            if(valpath.substring(0,15)==='_ActiveFilters.') {
+              var instance=self.GetFieldByID(valpath.substring(7));
+              if(ngIsFieldDef(instance))
+              {
+                instance.__Loading=true;
+                try
+                {
+                  if((deserialize)&&(typeof instance.Deserialize === 'function')) {
+                    setval=instance.Deserialize(setval);
+                  }
+                  else
+                  {
+                    if(typeof instance.TypedValue === 'function')
+                      setval=instance.TypedValue(setval);
+                  }
+                }
+                catch(e)
+                {
+                  // keep original, dont propagate errors on _ActiveFilters
+                }
+                if(ko.isObservable(instance.OriginalValue)) instance.OriginalValue(setval);
+                delete instance.__Loading;
+              }
+            }
+            return setval;
+          }
+          ret=ng_CallParent(this,'SetValues',arguments);
+        } finally {
+          this.OnSetValue=origonsetvalue;
+        }
+      }
+      else ret=ng_CallParent(this,'SetValues',arguments);
+      return ret;
+    }
+
+    function ngdsvm_GetValues(writableonly, valuenames, errors, convtimestamps, serialize)
+    {
+      var ret;      
+      if((ng_IsObjVar(this.ViewModel))&&(ng_IsObjVar(this.ViewModel._ActiveFilters))) {
+        var origongetvalue=this.OnGetValue;
+        try {
+          var self=this;
+          this.OnGetValue=function(c,val,instance, valpath, errors)
+          {
+            if(origongetvalue) val=origongetvalue.apply(self, arguments);
+            if(valpath.substring(0,15)==='_ActiveFilters.') {
+              var instance=self.GetFieldByID(valpath.substring(7));
+              if(ngIsFieldDef(instance))
+              {
+                instance.__Saving=true;
+                try
+                {
+                  if((serialize)&&(typeof instance.Serialize === 'function'))
+                    val=instance.Serialize(val);
+                  else
+                  {
+                    if(typeof instance.TypedValue === 'function')
+                      val=instance.TypedValue(val);
+                  }
+                }
+                catch(e)
+                {
+                  // keep original, dont propagate errors on _ActiveFilters
+                }
+                delete instance.__Saving;
+              }
+            }
+            return val;
+          }
+          ret=ng_CallParent(this,'GetValues',arguments);
+        } finally {
+          this.OnGetValue=origongetvalue;
+        }
+      } else ret=ng_CallParent(this,'GetValues',arguments);
+      return ret;
+    }
+
     ngRegisterControlType('ngSysDataSetViewModel',(function()
     {
       var fc=function(def, ref, parent) {
@@ -1141,7 +1228,10 @@ ngUserControls['viewmodel_controls'] = {
             GetColumnDefs: ngdsvm_GetColumnDefs,
             GetFilterDefs: ngdsvm_GetFilterDefs,
             IsAllowedSortBy: ngdsvm_IsAllowedSortBy,
-            GetActiveFilterValues: ngdsvm_GetActiveFilterValues
+            GetActiveFilterValues: ngdsvm_GetActiveFilterValues,
+
+            SetValues: ngdsvm_SetValues,
+            GetValues: ngdsvm_GetValues
           },
           OverrideEvents: {
             OnDoCommand: ngdsvm_DoCommand,
